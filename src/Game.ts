1b8121e8c21224077c5eb07c19c3e88bc5f21547
Akmal Anvarov <akmal@anvarov.dev>
2022-01-20 13:20:12 -0500

    rewrite game logic

--- src/Game.ts
+++ src/Game.ts
@@ -1,81 +1,204 @@
-// interface IGame {
-// 	player1: Player,
-// 	player2: Player,
-// 	play(): void,
-// 	generateDeck(): Record<string, number>,
-// 	shuffleDeck(deck: Array<string>): Array<string>
-// 	calculateWinner(): void
-// }
-
-import shuffle from "./utils";
-
 class Game {
-	cardRanks: Record<string, number>;
-	deck: Array<string>;
-	board: Array<string>
-	// history: Array<Record<string, number>>;
-	gameStatus: "war" | "nowar";
-	player1: Player;
-	player2: Player;
-	shuffleDeck = shuffle
-	calculateWinner(): void {
-			
-	}
-	generateDeck = (): Record<string, number> => {
-		const suites = ['D', 'C', 'H', 'S']
-		const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-		const deck: Record<string, number> = {}
-		for (let i = 0; i < ranks.length; i++) {
-			for (let j = 0; j < suites.length; j++) {
-				deck[ranks[i] + suites[j]] = i
-			}
-		}
-		return deck
-	}
-	play(): void {
-		this.player1.cards = this.deck.slice(0, 27)
-		this.player2.cards = this.deck
-		while (this.player1.cards.length === 52 || this.player2.cards.length === 52) {
-			if (this.gameStatus === 'nowar') {
-				this.board.push(...this.player1.takeOneCard())
-				this.board.push(...this.player2.takeOneCard())
-				this.calculateWinner()
-			}
-			if (this.gameStatus === 'war') {
-				this.board.push(this.player1.takeThreeCards())
-				this.board.push(this.player2.takeThreeCards())
-				this.calculateWinner()
-			}
-			this.shuffleDeck(this.deck)
-		}
-	}
-	constructor(player1: Player, player2: Player) {
-		this.player1 = player1
-		this.player2 = player2
-		this.deck = this.shuffleDeck(Array.from(Object.keys(this.generateDeck())))
-		this.cardRanks = this.generateDeck()
-		this.board = []
-			// this.history = []
-	}
+  private state: State;
+  constructor(state: State) {
+    this.setState(state);
+  }
+
+  public setState(state: State): void {
+    this.state = state;
+    this.state.setGame(this);
+  }
+
+  public getState(): State {
+    return this.state;
+  }
+
+  public calculateWinner(board: string[]): void {
+    if (board[board.length - 1] > board[board.length - 2]) {
+      this.state.setWarState("nowar");
+      this.state.getPlayerTwo().setFaceUpDeck(board);
+    } else if (board[board.length - 1] < board[board.length - 2]) {
+      this.state.setWarState("nowar");
+      this.state.getPlayerOne().setFaceUpDeck(board);
+    } else {
+      this.state.setWarState("war");
+    }
+  }
+  public play(): void {
+    const playerOne = this.state.getPlayerOne();
+    const playerTwo = this.state.getPlayerTwo();
+    if (this.state.getWarState() === "nowar") {
+      const playerOneCard = playerOne.takeOneCard();
+      if (playerOneCard.length === 0) {
+        this.state.setWinner(playerTwo);
+        return;
+      } else {
+        this.state.setBoard(playerOneCard);
+      }
+      const playerTwoCard = playerTwo.takeOneCard();
+      if (playerTwoCard.length === 0) {
+        this.state.setWinner(playerTwo);
+        return;
+      } else {
+        this.state.setBoard(playerTwoCard);
+      }
+      this.calculateWinner(this.state.getBoard());
+      this.state.setBoard([]);
+    } else {
+      let playerOneCards = playerOne.takeThreeCards();
+      if (playerOneCards.length === 0) {
+        this.state.setWinner(playerTwo);
+      } else {
+        this.state.setBoard(playerOneCards);
+      }
+      let playerTwoCards = playerTwo.takeThreeCards();
+      if (playerTwoCards.length === 0) {
+        this.state.setWinner(playerOne);
+      } else {
+        this.state.setBoard(playerTwoCards);
+      }
+      playerOneCards = playerOne.takeOneCard();
+      if (playerOneCards.length === 0) {
+        this.state.setWinner(playerTwo);
+      } else {
+        this.state.setBoard(playerOneCards);
+      }
+      playerTwoCards = playerTwo.takeOneCard();
+      if (playerTwoCards.length === 0) {
+        this.state.setWinner(playerOne);
+      } else {
+        this.state.setBoard(playerTwoCards);
+      }
+    }
+    this.calculateWinner(this.state.getBoard());
+  }
 }
 
-export default Game
-// class Game {
-// 	play(){
-// 		player1.cards = deck.slice(0,27)
-// 		player2.cards = deck
-// 		while (player1.cards.length === 52 || player2.cards.length === 52) {
-// 			if (gameStatus === 'nowar'){
-// 				player1.takeOneCard()
-// 				player2.takeOneCard()
-// 				calculateWinner(player1, player2)
-// 			}
-// 			if (gameStatus === 'war'){
-// 				player1.takeThreeCards()
-// 				player2.takeThreeCards()
-// 				calculateWinner(player1, player2)
-// 			}
-// 			shuffleDeck()
-// 		}
-// 	}
-// }
+class Player {
+  private faceDownDeck: string[];
+  private faceUpDeck: string[];
+  private name: string;
+  public constructor(name: string, deck: string[]) {
+    this.name = name;
+    this.faceDownDeck = deck;
+    this.faceUpDeck = [];
+  }
+  public getPlayerName(): string {
+    return this.name;
+  }
+  public takeOneCard(): string[] {
+    if (this.faceDownDeck.length === 0) {
+      this.fromFaceUpDeckToFaceDownDeck();
+    }
+    if (this.faceDownDeck.length > 0) {
+      return [this.faceDownDeck.pop()];
+    } else {
+      return [];
+    }
+  }
+  public takeThreeCards(): string[] {
+    if (this.faceDownDeck.length < 3) {
+      this.fromFaceUpDeckToFaceDownDeck();
+    }
+    if (this.faceDownDeck.length < 3) {
+      return [];
+    }
+    const cards = [];
+    cards.push(this.faceDownDeck.pop());
+    cards.push(this.faceDownDeck.pop());
+    cards.push(this.faceDownDeck.pop());
+    return cards;
+  }
+
+  public fromFaceUpDeckToFaceDownDeck(): void {
+    this.shuffleCards(this.faceUpDeck);
+    this.setFaceDownDeck(this.faceUpDeck);
+    this.setFaceUpDeck([]);
+  }
+
+  public setFaceUpDeck(cards: string[]): void {
+    this.faceUpDeck.push(...cards);
+  }
+
+  public getFaceUpDeck(): string[] {
+    return this.faceUpDeck;
+  }
+  public setFaceDownDeck(cards: string[]): void {
+    this.faceDownDeck.push(...cards);
+  }
+
+  public getfaceDownDeck(): string[] {
+    return this.faceDownDeck;
+  }
+
+  public shuffleCards(deck: string[]): void {
+    let currentIndex = deck.length,
+      randomIndex;
+    while (currentIndex != 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [deck[currentIndex], deck[randomIndex]] = [
+        deck[randomIndex],
+        deck[currentIndex],
+      ];
+    }
+  }
+}
+class State {
+  protected Game: Game;
+  private playerOne: Player;
+  private playerTwo: Player;
+  private winner: undefined | Player;
+  private board: string[];
+  private warState: "war" | "nowar";
+  public constructor(board: string[], playerOne: Player, playerTwo: Player) {
+    this.board = board;
+    this.playerOne = playerOne;
+    this.playerTwo = playerTwo;
+    this.winner = undefined;
+    this.warState = "nowar";
+  }
+  public setGame(Game: Game) {
+    this.Game = Game;
+  }
+
+  public setBoard(board: string[]): void {
+    this.board = board;
+  }
+
+  public getBoard(): string[] {
+    return this.board;
+  }
+  public getWarState(): "war" | "nowar" {
+    return this.warState;
+  }
+
+  public setWarState(warState: "war" | "nowar"): void {
+    this.warState = warState;
+  }
+
+  public getPlayerOne(): Player {
+    return this.playerOne;
+  }
+  public getPlayerTwo(): Player {
+    return this.playerTwo;
+  }
+  public setWinner(winner: Player): void {
+    this.winner = winner;
+  }
+  public getWinner(): Player {
+    return this.winner;
+  }
+}
+
+const playerOne = new Player("Akmal", ["kq"]);
+const playerTwo = new Player("Anvarov", ["kq"]);
+
+const game = new Game(new State([], playerOne, playerTwo));
+while (game.getState().getWinner() === undefined) {
+  game.play();
+}
+console.log(game.getState().getWinner().getPlayerName());
